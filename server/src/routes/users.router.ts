34fbcf80158fbc401a9d578135b5425826dfd99b
Philip von Koss <phpmvk@gmail.com>
2023-05-10 11:57:39 +0200

    patch: changed login endpoint

--- server/src/routes/users.router.ts
+++ server/src/routes/users.router.ts
@@ -4,7 +4,7 @@ import { creatorLogin, updateCreator, deleteCreator } from '../controllers/users
 import { defaultLimiter } from '../middleware/rate-limiter';
 import { authenticateToken } from '../middleware/auth';
 
-router.post('/users/creator/add', defaultLimiter, authenticateToken, creatorLogin)
+router.post('/users/creator/login', defaultLimiter, authenticateToken, creatorLogin)
 router.patch('/users/creator/update', defaultLimiter, authenticateToken, updateCreator)
 router.delete('/users/creator/delete', defaultLimiter, authenticateToken, deleteCreator)
 
