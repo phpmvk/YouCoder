30c2d8e6a5790212bf694942c4cd18910a870d65
Jordan Rollins <carpeomniavino@gmail.com>
2023-05-10 09:14:51 +0200

    Video Page Structure

--- client/src/pages/Video.tsx
+++ client/src/pages/Video.tsx
@@ -20,15 +20,15 @@ const VideoPage: React.FC<VideoPageProps> = ({}) => {
     
 
     <div className="bg-bg-pri relative flex justify-center items-center p-5 overflow-y-scroll">
-      <div className=" bg-white scale-85 border-solid p-2 mx-auto top-[20px] mb-1 rounded-lg overflow-y-scroll pb-[250px]">
+      <div className=" bg-white scale-85 border-solid p-2 mx-auto top-[20px] mb-1 rounded-lg overflow-y-scroll pb-[10px]">
         <PlaybackEditor />
-        <div className="w-full bg-gray-800 h-[120px] bottom-[20px] mt-[10px] rounded-lg">
+        <div className="w-full bg-orange-800 h-[400px] bottom-[20px] mt-[10px] rounded-lg">
           <div className="flex h-full">
             <div className="flex justify-center items-center">
               {/* Black div wrapper */}
-              <div className="bg-gray-800 w-[200px] h-[120px] rounded-lg ">
-                <img className="fixed bg-green-500 h-[80px] p-4 w-[80px] flex-shrink-0 mt-1 ml-[60px] rounded-full border-2 border-bg-alt"></img>
-                <div className="bg-gray-700 h-[40px] p-3 w-full flex-shrink-0 mt-20 rounded-lg text-gray-200 text-xl flex justify-center items-center">
+              <div className="bg-red-800 w-[200px] h-[400px] rounded-lg ">
+                <img className="bg-green-500 h-[100px] p-4 w-[100px] flex-shrink-0  ml-[50px] rounded-full border-2 border-bg-alt"></img>
+                <div className="bg-blue-700 h-[40px] p-3 w-full flex-shrink-0 rounded-lg text-gray-200 text-xl flex justify-center items-center">
                   {recording.creator}
                 </div>
               </div>
