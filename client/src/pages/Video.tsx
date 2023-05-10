aeab54984e39cfe1df517339bb2da111ee2ab0c5
Jordan Rollins <carpeomniavino@gmail.com>
2023-05-10 09:05:26 +0200

    Video Player Updates

--- client/src/pages/Video.tsx
+++ client/src/pages/Video.tsx
@@ -8,18 +8,19 @@ interface VideoPageProps {}
 
 
 const recording = {title: 'Javascript Functions',
-description: 'Follow me as I explain all-things JavaScript'};
+description: 'Follow me as I explain all-things JavaScript',
+creator: 'Someones Name',};
  
 
 const VideoPage: React.FC<VideoPageProps> = ({}) => {
   return (
-    <div className="min-h-screen overflow-y-hidden bg-bg-pri">
+    <div className="min-h-screen bg-bg-pri">
   <div>
     <TopNavBar />
     
 
-    <div className="bg-bg-pri relative flex justify-center items-center h-screen p-5">
-      <div className="fixed bg-white scale-85 border-solid p-2 mx-auto top-[20px] mb-1 rounded-lg">
+    <div className="bg-bg-pri relative flex justify-center items-center p-5 overflow-y-scroll">
+      <div className=" bg-white scale-85 border-solid p-2 mx-auto top-[20px] mb-1 rounded-lg overflow-y-scroll pb-[250px]">
         <PlaybackEditor />
         <div className="w-full bg-gray-800 h-[120px] bottom-[20px] mt-[10px] rounded-lg">
           <div className="flex h-full">
@@ -28,7 +29,7 @@ const VideoPage: React.FC<VideoPageProps> = ({}) => {
               <div className="bg-gray-800 w-[200px] h-[120px] rounded-lg ">
                 <img className="fixed bg-green-500 h-[80px] p-4 w-[80px] flex-shrink-0 mt-1 ml-[60px] rounded-full border-2 border-bg-alt"></img>
                 <div className="bg-gray-700 h-[40px] p-3 w-full flex-shrink-0 mt-20 rounded-lg text-gray-200 text-xl flex justify-center items-center">
-                  Someones Name
+                  {recording.creator}
                 </div>
               </div>
               {/* End of black div wrapper */}
