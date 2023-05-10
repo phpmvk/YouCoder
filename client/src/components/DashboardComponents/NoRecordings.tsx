3f63e89e14890c0945b3fa41211cd0cee8209406
Michael Epelboim <mepelboim@outlook.com>
2023-05-10 18:47:00 +0200

    typewriter effect

--- client/src/components/DashboardComponents/NoRecordings.tsx
+++ client/src/components/DashboardComponents/NoRecordings.tsx
@@ -1,15 +1,25 @@
+import Typewriter from 'typewriter-effect';
+import { useAppSelector } from '../../redux/hooks';
 interface NoRecordingsProps {}
 
 const NoRecordings: React.FC<NoRecordingsProps> = ({}) => {
+  const user = useAppSelector((state) => state.user);
   return (
     <>
       <div className='w-10/12 h-[60vh] bg-gradient-to-r from-white via-bg-alt to-bg-sec mx-auto rounded-md flex items-center justify-center p-3 relative'>
         <div className='w-full h-[57vh] bg-bg-pri m-auto rounded-lg flex items-center justify-center p-5 flex-col'>
-          <div className='text-white text-7xl blinking-cursor'>
-            time to record
+          <div className='text-white text-7xl'>
+            <Typewriter
+              options={{
+                strings: [`Hi ${user.shortName}`, "it's time", 'to record!'],
+                autoStart: true,
+                loop: true,
+                wrapperClassName: 'font-console',
+              }}
+            />
           </div>
           <div className='absolute top-10 mx-auto text-white'>
-            Make your first video
+            click the button above to get started
           </div>
         </div>
       </div>
