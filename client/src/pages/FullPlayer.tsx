564f853b69f333116d8a651390d77071640c243f
Jordan Rollins <carpeomniavino@gmail.com>
2023-05-12 16:35:30 +0200

    vid page edits

--- client/src/pages/FullPlayer.tsx
+++ client/src/pages/FullPlayer.tsx
@@ -15,7 +15,6 @@ interface FullPlayerPageProps {
   recordingData: Recording;
 }
 
-
 const recording = {
   title: "Javascript Functions Explained Pt. 2",
   description:
@@ -23,7 +22,6 @@ const recording = {
   creator: "Michael ",
   subs: "2.3K",
 };
-
 
 const JavaScriptSnippet = `
 // Simple addition function
@@ -100,11 +98,6 @@ function power(base, exponent) {
 
 `;
 
-
-
-
-
-
 const FullPlayerPage: React.FC<FullPlayerPageProps> = ({ recordingData }) => {
   return (
     <div className="min-h-screen bg-bg-pri min-w-[720px]">
@@ -126,93 +119,92 @@ const FullPlayerPage: React.FC<FullPlayerPageProps> = ({ recordingData }) => {
                   </div>
                   <div className="bg-bg-pri flex flex-row">
                     <div className="flex flex-row bg-bg-pri w-full h-[250px]">
+                      {/* ---------Left Card */}
+                      <div className=" mx-2 w-[20vw] px-10 h-56 max-w-sm bg-white border border-gray-200 rounded-xl shadow dark:bg-bg-pri dark:border-gray-700">
+                        <div className="flex justify-end px-4 pt-4"></div>
+                        <div className="flex flex-col items-center pb-10">
+                          <img
+                            className="w-20 h-20 mb-3 rounded-xl shadow-lg"
+                            src="./../../public/avatar.webp"
+                            alt=""
+                          />
+                          <h5 className=" text-xl font-medium text-gray-900 dark:text-white">
+                            {recording.creator}
+                          </h5>
+                          <Chip
+                            className="!bg-white/10 !text-gray-200 !text-sm !stroke-white !rounded-none w-full"
+                            label={`${recording.subs} Subscribers `}
+                          />
+                          <div className="flex space-x-3 mt-2">
+                            <Button
+                              variant="outlined"
+                              size="small"
+                              className="!rounded-xl !border-bg-alt !text-gray-200 align-middle h-1/2"
+                            >
+                              <AddIcon />
+                              Subscribe
+                            </Button>
+                          </div>
+                        </div>
+                      </div>
+                      {/* Middle Card -----------------------------------   */}
 
+                      <div className="flex mx-2 w-full py-4 h-56 left bg-white border border-gray-200 rounded-xl shadow dark:bg-bg-pri dark:border-gray-700 overflow-hidden text-white">
+                        <div className="flex justify-end px-4 pt-4">
+                          <p className="overflow-ellipsis ">
+                            {recording.description}
+                            
+                          </p>
+                        </div>
+                      </div>
 
+                      {/* ------Right Side Card */}
 
-
-                    <div className=" mx-2 w-[20vw] px-10 h-56 max-w-sm bg-white border border-gray-200 rounded-xl shadow dark:bg-bg-pri dark:border-gray-700">
-    <div className="flex justify-end px-4 pt-4">
-        
-        
-      
-    </div>
-    <div className="flex flex-col items-center pb-10">
-        <img className="w-20 h-20 mb-3 rounded-xl shadow-lg" src="./../../public/avatar.webp" alt=""/>
-        <h5 className=" text-xl font-medium text-gray-900 dark:text-white">{recording.creator}</h5>
-        <Chip
-                          className="!bg-white/10 !text-gray-200 !text-sm !stroke-white !rounded-none w-full"
-                          label={`${recording.subs} Subscribers `}
-                        />
-        <div className="flex space-x-3 mt-2">
-        <Button
-                          variant="outlined"
-                          size="small"
-                          className="!rounded-xl !border-bg-alt !text-gray-200 align-middle h-1/2"
-                        >
-                          <AddIcon />
-                          Subscribe
-                        </Button>
-            
-        </div>
-    </div>
-</div>
-{/* second -----------------------------------   */}
-
-<div className="flex mx-2 w-full py-4 h-56 left bg-white border border-gray-200 rounded-xl shadow dark:bg-bg-pri dark:border-gray-700 overflow-hidden text-white">
-    <div className="flex justify-end px-4 pt-4">
-        <p className="overflow-ellipsis ">{recording.description}{recording.description}</p>
-    </div>
-</div>
-
-{/* ------third */}
-
-<div className=" mx-2 w-[20vw] px-10 h-56 max-w-sm bg-white border border-gray-200 rounded-xl shadow dark:bg-bg-pri dark:border-gray-700">
-    <div className="flex py-5">
-        
-    
-                        <Stack className="mx-auto" direction="column" spacing={2}>
-                          <Button
-                            variant="outlined"
-                            size="small"
-                            className="!rounded-xl !border-bg-alt !text-gray-200 whitespace-nowrap h-1/2 align-middle"
+                      <div className=" mx-2 w-[20vw] md:px-10 sm:px-auto h-56 max-w-sm bg-white border border-gray-200 rounded-xl shadow dark:bg-bg-pri dark:border-gray-700">
+                        <div className="flex py-5 sm:px-0">
+                          <Stack
+                            className="mx-auto max-w-sm"
+                            direction="column"
+                            spacing={2}
                           >
-                            <ReplyIcon className="transform scale-x-[-1]" />
-                            share
-                          </Button>
+                            <Button
+                              variant="outlined"
+                              size="small"
+                              className="!rounded-xl !border-bg-alt !text-gray-200 whitespace-nowrap h-1/2 align-middle"
+                            >
+                              <ReplyIcon className="transform scale-x-[-1]" />
+                              share
+                            </Button>
 
-                          <Button
-                            variant="outlined"
-                            size="small"
-                            className="!rounded-xl !border-bg-alt !text-gray-200 whitespace-nowrap h-1/2"
-                          >
-                            <AddIcon />
-                            add to list
-                          </Button>
-                          <Button
-                            variant="outlined"
-                            size="small"
-                            className="!rounded-xl !border-bg-alt !text-gray-200 whitespace-nowrap h-1/2"
-                          >
-                            <AddIcon />
-                            add to list
-                          </Button>
-                          <Button
-                            variant="outlined"
-                            size="small"
-                            className="!rounded-xl !border-bg-alt !text-gray-200 whitespace-nowrap h-1/2"
-                          >
-                            <AddIcon />
-                            add to list
-                          </Button>
-                        </Stack>
-                      
-      
-    </div>
-    
-</div>
+                            <Button
+                              variant="outlined"
+                              size="small"
+                              className="!rounded-xl !border-bg-alt !text-gray-200 whitespace-nowrap h-1/2"
+                            >
+                              <AddIcon />
+                              add to list
+                            </Button>
+                            <Button
+                              variant="outlined"
+                              size="small"
+                              className="!rounded-xl !border-bg-alt !text-gray-200 whitespace-nowrap h-1/2"
+                            >
+                              <AddIcon />
+                              add to list
+                            </Button>
+                            <Button
+                              variant="outlined"
+                              size="small"
+                              className="!rounded-xl !border-bg-alt !text-gray-200 whitespace-nowrap h-1/2"
+                            >
+                              <AddIcon />
+                              add to list
+                            </Button>
+                          </Stack>
+                        </div>
+                      </div>
 
-
-{/* <div className=" mx-10 w-full px-10 h-56 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-red-800 dark:border-gray-700">
+                      {/* <div className=" mx-10 w-full px-10 h-56 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-red-800 dark:border-gray-700">
     <div className="flex justify-end px-4 pt-4">
         
         
@@ -239,18 +231,9 @@ const FullPlayerPage: React.FC<FullPlayerPageProps> = ({ recordingData }) => {
     </div>
 </div> */}
 
-
-{/* <div className=" bg-bg-sec text-gray-200 text-l text-left p-4  pb-10 mt-4 rounded-b-xl w-full">
+                      {/* <div className=" bg-bg-sec text-gray-200 text-l text-left p-4  pb-10 mt-4 rounded-b-xl w-full">
                       {recording.description}
                     </div> */}
-
-
-
-
-
-
-
-
 
                       {/* <div
                         className="ml-16 mr-5 pic h-[80px] w-[80px] flex-shrink-0 rounded-3xl border-2 border-bg-alt bg-cover bg-center bg-red-900"
@@ -278,13 +261,6 @@ const FullPlayerPage: React.FC<FullPlayerPageProps> = ({ recordingData }) => {
                         </Button>
                       </div> */}
 
-
-
-
-
-
-                      
-
                       {/* <div className="my-auto justify-end pl-10">
                         <Stack className="mx-10" direction="column" spacing={3}>
                           <Button
@@ -307,43 +283,25 @@ const FullPlayerPage: React.FC<FullPlayerPageProps> = ({ recordingData }) => {
                         </Stack>
                       </div> */}
                     </div>
-                    
                   </div>
                   <div className="flex  w-full  px-4 text-left  bg-bg-pri  overflow-hidden overflow-y-scroll text-3xl text-gray-200">
-    <div className="flex justify-end px-4 py-2">Code
-        <p className="overflow-ellipsis ">
-
-
-       
-
-
-
-
-
-          
-        </p>
-    </div>
-</div>
-{/* code block------ */}
+                    <div className="flex justify-end px-4 py-2">
+                      Code
+                      <p className="overflow-ellipsis "></p>
+                    </div>
+                  </div>
+                  {/* code block------ */}
                   <div className="flex  w-4/5 mx-2  h-full text-left bg-white border border-gray-200 rounded-xl shadow dark:bg-bg-pri dark:border-gray-700 overflow-hidden overflow-y-scroll text-white">
-    <div className="flex justify-end px-4 pt-4">
-        <p className="overflow-ellipsis ">
-
-
-        <div>
-      <pre>
-        <code>{JavaScriptSnippet}</code>
-      </pre>
-    </div>
-
-
-
-
-
-          
-        </p>
-    </div>
-</div>
+                    <div className="flex justify-end px-4 pt-4">
+                      <p className="overflow-ellipsis ">
+                        <div>
+                          <pre>
+                            <code>{JavaScriptSnippet}</code>
+                          </pre>
+                        </div>
+                      </p>
+                    </div>
+                  </div>
                   <div className="w-full h-[150px] bg-bg-pri">
                     {/* <div className="w-full bg-bg-sec text-gray-200 text-l text-left p-4 pl-10  pb-10 mt-4 rounded-b-xl">
                       {recording.description}
