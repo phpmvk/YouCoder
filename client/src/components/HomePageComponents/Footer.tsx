cbe2482405d91151b00878d18833f45331ebaee9
Jordan Rollins <carpeomniavino@gmail.com>
2023-05-16 09:43:18 +0200

    wip

--- client/src/components/HomePageComponents/Footer.tsx
+++ client/src/components/HomePageComponents/Footer.tsx
@@ -4,45 +4,6 @@ import { Box, Container, Grid, Typography } from '@mui/material';
 export const Footer: FC = (): ReactElement => {
   return (
 
-    // <Box
-    //   sx={{
-    //     width: '100%',
-    //     height: '4vh',
-    //     backgroundColor: 'black',
-    //     paddingTop: '0.25rem',
-    //     paddingBottom: '0.25rem',
-    //   }}
-    // >
-    //   <Container maxWidth='lg'>
-    //     <Grid
-    //       container
-    //       direction='column'
-    //       alignItems='center'
-    //     >
-    //       <Grid
-    //         item
-    //         xs={12}
-    //       >
-    //         <Typography
-    //           color='white'
-    //           variant='h5'
-    //         ></Typography>
-    //       </Grid>
-    //       <Grid
-    //         item
-    //         xs={12}
-    //       >
-    //         <Typography
-    //           color='white'
-    //           variant='subtitle1'
-    //         >
-    //           {`${new Date().getFullYear()} | YouCoder`}
-    //         </Typography>
-    //       </Grid>
-    //     </Grid>
-    //   </Container>
-    // </Box>
-
 <footer className="bg-bg-pri m-1 sticky bottom-0">
   <div className="w-full mx-auto max-w-screen-xl p-4 flex flex-wrap justify-between items-center text-sm">
     <div className="flex items-center text-gray-300 mb-2 sm:mb-0">
@@ -50,38 +11,9 @@ export const Footer: FC = (): ReactElement => {
       <a href="https://youcoder.io/" className="hover:underline">YouCoder</a>
       <span>. All Rights Reserved.</span>
     </div>
-    <ul className="flex items-center text-sm font-medium text-gray-200">
-      <li>
-        <a href="https://youcoder.io/team" className="hover:underline">Team</a>
-      </li>
-    </ul>
+    
   </div>
 </footer>
-
-
-    //<footer className='bg-bg-pri my-4 w-full pt-5 flex justify-between items-center text-sm text-gray-500 font-medium px-10'>
-    //  <span>
-    //    © 2023{' '}
-    //    <a
-     //     href='https://youcoder.io/'
-     //     className='hover:underline'
-    //    >
-     //     YouCoder
-     //   </a>
-     //   . All Rights Reserved.
-     // </span>
-    //  <ul>
-     //   <li>
-      //    <a
-      //      href='#'
-      //      className='hover:underline'
-       //   >
-       //     Contact
-      //    </a>
-      //  </li>
-    //  </ul>
-   // </footer>
-
   );
 };
 
--- client/src/pages/Team.tsx
+++ client/src/pages/Team.tsx
@@ -1,28 +1,37 @@
 import React from 'react'
+import TopNavBar from '../components/HomePageComponents/TopNavBar';
+import IconButton from '@mui/material/IconButton';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { Link } from 'react-router-dom';
 
 const getRandomPercentage = () => `${Math.floor(Math.random() * 100)}%`;
 
 const Team = () => {
   const teamMembers = [
-    { name: "Member 1", role: "Role 1", img: "path/to/image1.jpg" },
-    { name: "Member 2", role: "Role 2", img: "path/to/image2.jpg" },
-    { name: "Member 3", role: "Role 3", img: "path/to/image3.jpg" },
-    { name: "Member 4", role: "Role 4", img: "path/to/image4.jpg" },
+    { name: "Idar Nigatu", img: "https://avatars.githubusercontent.com/u/61595151?v=4", gh: "https://github.com/IdarDev"  },
+    { name: "Michael Epelboim", img: "https://avatars.githubusercontent.com/u/67226592?v=4", gh: "https://github.com/michaelito80us" },
+    { name: "Philip von Koss", img: "https://avatars.githubusercontent.com/u/123027089?v=4", gh: "https://github.com/phpmvk"},
+    { name: "Jordan Rollins", img: "https://avatars.githubusercontent.com/u/116223808?v=4", gh: "https://github.com/0xjcr" },
   ];
+
+  
+  
 
   return (
     <>
-      <div className="h-screen w-screen bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-bg-pri via-slate-900 to-bg-pri z-1 flex justify-center items-center text-gray-200">
+    <TopNavBar/>
+      <div className="h-screen w-screen bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-bg-pri via-white/10 to-bg-pri z-1 flex justify-center items-center text-gray-200">
         {[...Array(8)].map((_, index) => (
           <div
             key={index}
-            className={`absolute w-3 h-3 rounded-full animate-pulse ${index % 2 === 0 ? 'bg-gradient-to-r from-black to-red-500' : 'bg-gradient-to-r from-black to-white'} opacity-50`}
+            className={`absolute w-3 h-3 rounded-full z-0 ${index % 2 === 0 ? 'bg-gradient-to-r from-black to-red-500' : 'bg-gradient-to-r from-black to-white'} opacity-50`}
             style={{top: getRandomPercentage(), left: getRandomPercentage()}}
           />
         ))}
-
-        <h1 className="text-5xl font-bold mb-10 pr-16 text-white">Our Team</h1>
-
+        <div className="flex flex-col items-center">
+        <img src="/Layer-1.svg" alt="logo" className="w-[10vw]" />
+  <h1 className="text-2xl font-bold mb-10 p-16 my-10 text-grey-200 font-title z-10">The Team Behind YouCoder</h1>
+</div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5">
           {teamMembers.map((member, index) => (
             <div
@@ -33,10 +42,16 @@ const Team = () => {
                 <img
                   src={member.img}
                   alt={member.name}
-                  className="h-24 w-24 object-cover rounded-full mb-4"
+                  className="h-20 w-20 object-cover rounded-full mb-4"
                 />
-                <h2 className="text-2xl font-bold text-white">{member.name}</h2>
-                <p className="text-xl text-white">{member.role}</p>
+                <h2 className=" sm:text-sm md:text-md text-lg text-white whitespace-nowrap z-10">{member.name}</h2>
+                <div>
+                  <a href={member.gh} target="_blank" rel="noopener noreferrer">
+                    <IconButton className="!text-white">
+                      <GitHubIcon/>
+                    </IconButton>
+                  </a>
+                </div>
               </div>
             </div>
           ))}
@@ -45,5 +60,6 @@ const Team = () => {
     </>
   );
 };
+
 
 export default Team;
