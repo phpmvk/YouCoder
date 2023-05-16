6ef77ab69d9b70a295b5e0ed346323cd674193b8
Philip von Koss <phpmvk@gmail.com>
2023-05-16 11:15:49 +0200

    feat: response headers now inlclude information about rate-limit where needed

--- server/src/middleware/rate-limiter.ts
+++ server/src/middleware/rate-limiter.ts
@@ -3,13 +3,15 @@ import rateLimit  from 'express-rate-limit';
 export const consoleRouteLimiter = rateLimit({
   windowMs: 30 * 1000,
   max: 5,
-  message: { error: 'Rate limit reached. Please try again later' }
+  message: { error: 'Rate limit reached. Please try again later' },
+  standardHeaders: true,
 })
 
 export const defaultLimiter = rateLimit({
   windowMs: 60 * 1000,
   max: 10,
-  message: { error: 'Rate limit reached. Please try again later' }
+  message: { error: 'Rate limit reached. Please try again later' },
+  standardHeaders: true,
 })
 
 export const likeRecordingLimiter = rateLimit({
