7952bbc857f2fb6103c1f0cda524cb01623132c1
Philip von Koss <phpmvk@gmail.com>
2023-05-17 18:02:30 +0200

    fix: cors logic rectified

--- server/src/middleware/cors.ts
+++ server/src/middleware/cors.ts
@@ -4,18 +4,15 @@ import { allowedOrigins } from "../config";
 export async function corsMiddleware(req: Request, res: Response, next: NextFunction) {
   const origin = req.headers.origin;
 
-  if (allowedOrigins.includes(origin!)) {
-    res.setHeader('Access-Control-Allow-Origin', origin!);
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  if (origin && allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, PUT, PATCH, POST, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
     if (req.method === 'OPTIONS') {
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-      return res.status(200).end();
+      res.sendStatus(204);
+      return;
     }
-  }
-
-  if (!allowedOrigins.includes(origin!)) {
-    return res.status(403).send({ error: 'Access denied due to CORS restrictions.' });
   }
 
   next();
