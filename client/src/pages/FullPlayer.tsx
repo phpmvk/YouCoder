070203d8ddca95477cfca612f81566d1067cdbaa
Michael Epelboim <mepelboim@outlook.com>
2023-05-17 23:17:17 +0200

    social media links in the player page

--- client/src/pages/FullPlayer.tsx
+++ client/src/pages/FullPlayer.tsx
@@ -16,12 +16,11 @@ import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import TwitterIcon from '@mui/icons-material/Twitter';
+import { Link } from 'react-router-dom';
 
 interface FullPlayerPageProps {
   recordingData: Recording;
 }
-
-
 
 interface Social {
   name: string;
@@ -29,9 +28,7 @@ interface Social {
 }
 
 const FullPlayerPage: React.FC<FullPlayerPageProps> = ({ recordingData }) => {
-
-console.log('recordingData:--->', recordingData)
-
+  console.log('recordingData:--->', recordingData);
 
   return (
     <div className='min-h-screen bg-bg-pri min-w-[720px] px-20 flex flex-col justify-center items-center'>
@@ -55,88 +52,75 @@ console.log('recordingData:--->', recordingData)
                     {recordingData.title}
                   </div>
                   <div className='bg-bg-pri flex flex-row border border-gray-600 rounded-lg p-3 pt-4 h-60 mb-4'>
-                    
-                      {/* ---------Left Card */}
-                      <div className='flex flex-col items-center mx-1 min-w-36 max-w-[20vw] '>
-                        <div className='h-36 w-36 md:h-44 md:w-44 rounded-xl shadow bg-bg-pri flex items-center justify-center md:-mt-4'>
-                          <div className='flex flex-col items-center'>
-                            <img
-                              className='w-20 h-20 !min-w-20 mb-3 rounded-full shadow-lg'
-                              src={recordingData.creator!.picture}
-                              alt=''
-                            />
-                            <h5 className=' text-sm md:text-lg  font-medium text-white text-ellipsis line-clamp-2'>
+                    {/* ---------Left Card */}
+                    <div className='flex flex-col items-center mx-1 min-w-36 max-w-[20vw] '>
+                      <div className='h-36 w-36 md:h-44 md:w-44 rounded-xl shadow bg-bg-pri flex items-center justify-center md:-mt-4'>
+                        <div className='flex flex-col items-center'>
+                          <img
+                            className='w-20 h-20 !min-w-20 mb-3 rounded-full shadow-lg'
+                            src={recordingData.creator!.picture}
+                            alt=''
+                          />
+                          <Link
+                            to={`/discover?user=${recordingData.creator?.uid}`}
+                          >
+                            <h5 className='hover:underline text-sm md:text-lg  font-medium text-white text-ellipsis line-clamp-2'>
                               {recordingData.creator?.display_name}
                             </h5>
-                          </div>
+                          </Link>
                         </div>
-
-
-
-                        {/* <Button
-                          variant='contained'
-                          size='small'
-                          onClick={(e) => {
-                            navigator.clipboard.writeText(
-                              recordingData.full_link
-                            );
-                            toast.success('Link copied to clipboard');
-                          }}
-                          className='!rounded-full !bg-bg-muigrey !border-gray-700 w-36 md:w-44 !text-gray-100 whitespace-nowrap h-8 align-middle active:ring-1 active:ring-bg-alt hover:!bg-gray-700'
-                        >
-                           <ContentCopyIcon className='scale-75'/> 
-                          copy link
-                        </Button> */}
-
-{ }
-
-<div className='flex justify-center space-x-3 lg:space-x-4 scale-150 mt-2'>
-  {recordingData.creator.socials.length > 0 && recordingData.creator.socials.some((social: Social) => social.name === 'github') ? (
-    <a href={recordingData.creator.socials.find((social: Social) => social.name === 'github').social}>
-    <IconButton aria-label="GitHub"><GitHubIcon className='!fill-white' /></IconButton>
-  </a>
-  ) : (
-    <GitHubIcon className='!fill-bg-gptdark'/>
-  )}
-
-  {recordingData.creator.socials.length > 0 && recordingData.creator.socials.some((social: Social) => social.name === 'youtube') ? (
-    <a href={recordingData.creator.socials.find((social: Social) => social.name === 'youtube').social}>
-    <IconButton aria-label="YouTube"><YouTubeIcon className='!fill-white'/></IconButton>
-  </a>
-  ) : (
-    <YouTubeIcon className='!fill-bg-gptdark'/>
-  )}
-
-  {recordingData.creator.socials.length > 0 && recordingData.creator.socials.some((social: Social) => social.name === 'twitter') ? (
-    <a href={recordingData.creator.socials.find((social: Social) => social.name === 'twitter').social}>
-    <IconButton aria-label="Twitter"><TwitterIcon className='!fill-white'/></IconButton>
-  </a>
-  ) : (
-    <TwitterIcon className='!fill-bg-gptdark'/>
-  )}
-</div>
-
-
-
                       </div>
+                      {}
 
-                      {/* Middle Card -----------------------------------   */}
+                      <div className='flex justify-around items-center space-x-3 lg:space-x-4 scale-150'>
+                        {recordingData.creator!.socials.github !== '' ? (
+                          <a href={recordingData.creator?.socials.github}>
+                            <IconButton aria-label='GitHub'>
+                              <GitHubIcon className='!fill-white' />
+                            </IconButton>
+                          </a>
+                        ) : (
+                          <GitHubIcon className='!fill-bg-gptdark' />
+                        )}
 
-                      <div className='flex-col mx-1 min-w-[60vw] max-w-[1000px] w-full py-4 ml-4 h-52 text-left  border-l  shadow bg-bg-pri border-gray-600 overflow-hidden text-white'>
-  <div className='px-5 pt-1 md:h-full max-w-[60vw] overflow-y-auto scrollbar-hide overflow-x-hidden' style={{wordWrap: 'break-word', whiteSpace: 'pre-wrap'}}>
-    <p>{recordingData.description}</p>
-  </div>
-</div>
+                        {recordingData.creator!.socials.youtube !== '' ? (
+                          <a href={recordingData.creator?.socials.youtube}>
+                            <IconButton aria-label='YouTube'>
+                              <YouTubeIcon className='!fill-white' />
+                            </IconButton>
+                          </a>
+                        ) : (
+                          <YouTubeIcon className='!fill-bg-gptdark' />
+                        )}
 
- {/* end of card-----------------------------------------------*/}
+                        {recordingData.creator!.socials.twitter !== '' ? (
+                          <a href={recordingData.creator!.socials.twitter}>
+                            <IconButton aria-label='Twitter'>
+                              <TwitterIcon className='!fill-white' />
+                            </IconButton>
+                          </a>
+                        ) : (
+                          <TwitterIcon className='!fill-bg-gptdark' />
+                        )}
+                      </div>
+                    </div>
 
-                     
+                    {/* Middle Card -----------------------------------   */}
 
-                      
-                    
+                    <div className='flex-col mx-1 min-w-[60vw] max-w-[1000px] w-full py-4 ml-4 h-52 text-left  border-l  shadow bg-bg-pri border-gray-600 overflow-hidden text-white'>
+                      <div
+                        className='px-5 pt-1 md:h-full max-w-[60vw] overflow-y-auto scrollbar-hide overflow-x-hidden'
+                        style={{
+                          wordWrap: 'break-word',
+                          whiteSpace: 'pre-wrap',
+                        }}
+                      >
+                        <p>{recordingData.description}</p>
+                      </div>
+                    </div>
+
+                    {/* end of card-----------------------------------------------*/}
                   </div>
-                  
-                 
                 </div>
 
                 <div className='flex md:flex-row'></div>
