ca08a0433d7337e038841f9ffe3a29d40192aebb
Philip von Koss <phpmvk@gmail.com>
2025-02-09 15:10:46 +0100

    Update config.ts
    fix: added new domain to config file

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,2 +1,2 @@
 export const PORT: number = +(process.env.PORT ?? 3001);
-export const allowedOrigins: string[] = ['http://localhost:5173', 'http://localhost:4173', 'https://youcoder.io']
+export const allowedOrigins: string[] = ['http://localhost:5173', 'http://localhost:4173', 'https://youcoder.io', 'https://youcoder.netlify.app']
